be3d5bff14403627b5778120fcdbca77016bf030
LucaBellomo <37655551+LucaBellomo@users.noreply.github.com>
2020-06-09 15:11:04 +0200

    Update jsPipe.js

--- jsPipe.js
+++ jsPipe.js
@@ -1,11 +1,10 @@
 const pipe = (init, ...functions) => {
-  if(functions.length === 0) throw new Error('The function must have some parameters');
-  if(functions.length === 1) return functions[0];
+  if (!init) throw new Error('The function pipe needs some arguments');
+  if (functions.length === 0) return init;
   return functions.reduce((acc, current) => current(acc), init);
 }
 
 const double = x => x * x;
 
-
 const result = pipe(double(2), double, double);
-console.log(result);
+console.log(result); // 256
